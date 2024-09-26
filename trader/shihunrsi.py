fe34a88f66c6ec12afa53255b9f308e33ed364da
outlier <outlier@chainer.tech>
2024-09-26 09:55:53 +0800

    feat:shihun RSI

---
+++ trader/shihunrsi.py
@@ -0,0 +1,118 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime
+import os.path
+import sys
+
+from backtrader import num2date
+
+from trader.binance.csvdata import BinanceCSVData
+from trader.utils import path
+
+import backtrader as bt
+
+
+class ShihunRSIStrategy(bt.Strategy):
+    params = (
+        ('confirm', 3),
+        ('period', 14),
+        ('overbought', 70),
+        ('oversold', 30),
+    )
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime[0]
+        dat = num2date(dt)
+        print(f"{dat}, {txt}")
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+        self.goldenFork = 0
+        self.deathFork = 0
+
+        self.rsi = bt.indicators.RSI(self.data.close, period=self.params.period)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    '买入, 价格: %.2f, 花费: %.2f, 手续费: %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('卖出, 价格: %.2f, 花费: %.2f, 手续费: %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('营业利润, 毛利润: %.2f, 净利润: %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        self.log('收盘价, %.2f' % self.dataclose[0])
+
+        if self.order:
+            return
+
+        if not self.position:
+            if self.rsi[0] > self.params.overbought:
+                self.sell()
+        else:
+            if self.rsi[0] < self.params.oversold:
+                self.buy()
+
+
+
+def shihunRSI(main=False):
+    cerebro = bt.Cerebro()
+
+    cerebro.addstrategy(ShihunRSIStrategy)
+
+    datapath = os.path.join(path.GetDatasDir(), 'ETHUSDT-1h-202301-202401.csv')
+
+    data = BinanceCSVData(
+        dataname=datapath,
+        fromdate=datetime.datetime(2023, 1, 1),
+        todate=datetime.datetime(2024, 1, 1),
+    )
+
+    cerebro.adddata(data)
+
+    cerebro.broker.setcash(100000.0)
+
+    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+    cerebro.broker.setcommission(commission=0.0)
+
+    print('\n初始资产: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('最终资产: %.2f' % cerebro.broker.getvalue())
+
+    if main:
+        cerebro.plot()
+
+if __name__ == '__main__':
+    shihunRSI(True)
