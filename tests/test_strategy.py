ac5278ee06fa0e2fa809dcaa5983f98e45efc925
outlier <outlier@chainer.tech>
2024-11-19 09:21:49 +0800

    test:ShihunMACD

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -2,6 +2,10 @@ from trader.app.app import App
 from trader.common.config import Config
 from trader.utils.trend import TrendType
 
+def test_ShihunMACD():
+    app = App()
+    if app.start(Config("ShihunMACD")):
+        app.stop()
 
 # MACD + RSI + BollingerBand from ShiHun only in the upward trend
 def test_ShihunMacdRsiBollingerBand_UP():
