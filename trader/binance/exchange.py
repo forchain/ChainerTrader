2906ee0fbf55269a2bb7db68c4225da36d022ae2
outlier <outlier@chainer.tech>
2024-11-21 10:09:25 +0800

    feat:binance exchange about start

--- trader/binance/exchange.py
+++ trader/binance/exchange.py
@@ -1,9 +1,25 @@
+from binance.spot import Spot as Client
+
+from trader.binance.restapi import get_restapi
 
 EXCHANGE_NAME = "BINANCE"
 
 class BinanceExchange:
-    def __init__(self):
-        pass
+    def __init__(self,log):
+        self.log=log
+        self.log.info(f"Init Exchange {self.name()}")
+
+        base_url=get_restapi(False)
+        self.spot_client=Client(base_url=base_url)
 
     def name(self):
-        return NAME
+        return EXCHANGE_NAME
+
+    def start(self):
+        self.log.info(f"Start {self.name()} exchange")
+
+        info = self.spot_client.exchange_info()
+        self.log.info(f"Get {self.name()} exchange_info:{info}")
+
+    def stop(self):
+        self.log.info(f"Stop {self.name()} exchange")
