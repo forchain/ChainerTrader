514352b8fb71445c4d1963de45d4e489e32e5165
outlier <outlier@chainer.tech>
2024-11-30 18:59:03 +0800

    feat:optimize task manager init

--- trader/app/task_manager.py
+++ trader/app/task_manager.py
@@ -1,13 +1,18 @@
-from trader.app.app import App
+from logging import Logger
+
+from trader.app.database_manager import DatabaseManager
 from trader.app.dynamic_task import DynamicTask
 from trader.app.static_task import StaticTask
+from trader.binance.exchange import BinanceExchange
+from trader.common.config import Config
 
 
 class TaskManager:
-    def __init__(self,app:App):
-        self.app=app
-        self.log = app.log()
-        self.cfg = app.config()
+    def __init__(self,cfg:Config,log:Logger,db_manager:DatabaseManager,exchange:BinanceExchange):
+        self.log = log
+        self.cfg = cfg
+        self.db_manager = db_manager
+        self.exchange = exchange
         self.log.info(f"Init TaskManager")
         self.tasks = []
 
@@ -19,7 +24,7 @@ class TaskManager:
         if self.cfg.data_file:
             self.tasks.append(StaticTask(self.cfg,self.log))
         if self.cfg.exchange:
-            self.tasks.append(DynamicTask(self.app))
+            self.tasks.append(DynamicTask(self.cfg,self.log,self.db_manager,self.exchange))
 
         for task in self.tasks:
             task.start()
