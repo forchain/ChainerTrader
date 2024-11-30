66797837430c8507085f3f9bc93cd43274fb10bc
outlier <outlier@chainer.tech>
2024-11-30 22:14:59 +0800

    feat:optimize binance data

--- trader/binance/data.py
+++ trader/binance/data.py
@@ -1,116 +1,29 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from trader.binance.csvdata import BinanceCSVData
+from datetime import datetime
+
+import backtrader as bt
+from backtrader import date2num
+
+from trader.utils.kline import Kline
 
 
-class BinanceData(BinanceCSVData):
-    params = (
-        ('proxies', {}),
-        ('period', 'd'),
-        ('reverse', False),
-        ('urlhist', ''),
-        ('urldown', ''),
-        ('retries', 3),
-    )
+class BinanceData(bt.feed.DataBase):
+    def __init__(self, data:[Kline]):
+        super().__init__()
+        self.data = data
+        self.index = 0
 
-    def start_v7(self):
-        try:
-            import requests
-        except ImportError:
-            msg = ('The new binance data feed requires to have the requests '
-                   'module installed. Please use pip install requests or '
-                   'the method of your choice')
-            raise Exception(msg)
-
-        self.error = None
-        url = self.p.urlhist.format(self.p.dataname)
-
-        sesskwargs = dict()
-        if self.p.proxies:
-            sesskwargs['proxies'] = self.p.proxies
-
-        crumb = None
-        sess = requests.Session()
-        sess.headers['User-Agent'] = 'chainer'
-        for i in range(self.p.retries + 1):  # at least once
-            resp = sess.get(url, **sesskwargs)
-            if resp.status_code != requests.codes.ok:
-                continue
-
-            txt = resp.text
-            i = txt.find('CrumbStore')
-            if i == -1:
-                continue
-            i = txt.find('crumb', i)
-            if i == -1:
-                continue
-            istart = txt.find('"', i + len('crumb') + 1)
-            if istart == -1:
-                continue
-            istart += 1
-            iend = txt.find('"', istart)
-            if iend == -1:
-                continue
-
-            crumb = txt[istart:iend]
-            crumb = crumb.encode('ascii').decode('unicode-escape')
-            break
-
-        if crumb is None:
-            self.error = 'Crumb not found'
-            self.f = None
-            return
-
-        crumb = urlquote(crumb)
-
-        # Try to download
-        urld = '{}/{}'.format(self.p.urldown, self.p.dataname)
-
-        urlargs = []
-        posix = date(1970, 1, 1)
-        if self.p.todate is not None:
-            period2 = (self.p.todate.date() - posix).total_seconds()
-            urlargs.append('period2={}'.format(int(period2)))
-
-        if self.p.todate is not None:
-            period1 = (self.p.fromdate.date() - posix).total_seconds()
-            urlargs.append('period1={}'.format(int(period1)))
-
-        intervals = {
-            bt.TimeFrame.Days: '1d',
-            bt.TimeFrame.Weeks: '1wk',
-            bt.TimeFrame.Months: '1mo',
-        }
-
-        urlargs.append('interval={}'.format(intervals[self.p.timeframe]))
-        urlargs.append('events=history')
-        urlargs.append('crumb={}'.format(crumb))
-
-        urld = '{}?{}'.format(urld, '&'.join(urlargs))
-        f = None
-        for i in range(self.p.retries + 1):  # at least once
-            resp = sess.get(urld, **sesskwargs)
-            if resp.status_code != requests.codes.ok:
-                continue
-
-            ctype = resp.headers['Content-Type']
-            if not ctype.startswith('text/'):
-                self.error = 'Wrong content type: %s' % ctype
-                continue  # HTML returned? wrong url?
-
-            try:
-                # r.encoding = 'UTF-8'
-                f = io.StringIO(resp.text, newline=None)
-            except Exception:
-                continue  # try again if possible
-
-            break
-
-        self.f = f
-
-    def start(self):
-        self.start_v7()
-
-        # Prepared a "path" file -  CSV Parser can take over
-        super(BinanceCSVData, self).start()
+    def _load(self):
+        if self.index >= len(self.data):
+            return False
+        kl = self.data[self.index]
+        self.lines.datetime[0] = date2num(datetime.fromtimestamp(kl.open_time))
+        self.lines.open[0] = kl.open
+        self.lines.high[0] = kl.high
+        self.lines.low[0] = kl.low
+        self.lines.close[0] = kl.close
+        self.lines.volume[0] = kl.volume
+        self.index += 1
+        return True
