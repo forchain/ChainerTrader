0d68d7770841992f73c8004fd12b9a98c1678064
outlier <outlier@chainer.tech>
2024-12-04 10:36:24 +0800

    feat:support update klines task

--- trader/common/common.py
+++ trader/common/common.py
@@ -1,4 +1,13 @@
+import time
+from logging import Logger
 
 
 class Context:
     running: bool = False
+
+def sleep(log:Logger,seconds,msg=None):
+    if msg:
+        log.debug(f"Waiting for {seconds} seconds for {msg}")
+    else:
+        log.debug(f"Waiting for {seconds} seconds")
+    time.sleep(seconds)
