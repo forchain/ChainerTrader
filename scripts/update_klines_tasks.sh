702a39a2e8f2f9b9a080e8101a64b6f997d3d99b
outlier <outlier@chainer.tech>
2024-12-25 11:46:02 +0800

    fix:scripts

--- scripts/update_klines_tasks.sh
+++ scripts/update_klines_tasks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-trader_dev --tasks=./update_klines_2829.json --db_uri="mongodb://localhost:27017/" --exchange=BINANCE --log_level=DEBUG --log_file
+trader_dev --tasks=./update_klines_2829.json --db_uri="mongodb://localhost:27017/" --exchange=BINANCE --log_level=DEBUG
 
 
---
+++ scripts/update_klines_tasks_100.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+trader_dev --tasks=./update_klines_100.json --db_uri="mongodb://localhost:27017/" --exchange=BINANCE --log_level=DEBUG
+
+
