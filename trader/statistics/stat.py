51c79ade8cc2eb31b64917335be38587d4443fa8
outlier <outlier@chainer.tech>
2024-12-27 09:52:38 +0800

    feat:add BackTraderStat

---
+++ trader/statistics/stat.py
@@ -0,0 +1,10 @@
+
+class BackTraderStat:
+    def __init__(self,strategy,symbol_interval,total_return_rate):
+        self.strategy=strategy
+        self.symbol_interval=symbol_interval
+        self.total_return_rate=total_return_rate
+
+class TraderStat:
+    def __init__(self):
+        pass
