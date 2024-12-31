b150b16f967fce9aa2e64b1faf2bcc15368b1b41
outlier <outlier@chainer.tech>
2024-12-31 12:30:38 +0800

    feat:optimize database config

--- scripts/update_klines_tasks.sh
+++ scripts/update_klines_tasks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-trader_dev --tasks=./update_klines.json --db_uri="mongodb://localhost:27017/" --exchange=BINANCE --log_level=DEBUG
+trader_dev --tasks=./update_klines.json --db --exchange=BINANCE --log_level=DEBUG
 
 
