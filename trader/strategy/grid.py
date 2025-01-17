45a8a9e864c5212d62a6189df8c5e62028eae39c
outlier <outlier@chainer.tech>
2025-01-17 12:13:22 +0800

    feat:add strategy grid

---
+++ trader/strategy/grid.py
@@ -0,0 +1,66 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import backtrader as bt
+
+from trader.strategy.base_strategy import BaseStrategy
+
+class GridStrategy(BaseStrategy):
+    params = (
+        ("grid_size", 20),
+        ("grid_levels", 10),
+        ("once_size", 20),
+    )
+
+    def __init__(self):
+        super().__init__()
+
+        self.grid_centers = []
+        self.buy_levels = []
+        self.sell_levels = []
+        self.order_dict = {}
+
+        # init grid
+        latest_price = self.data.close[-len(self.data)]
+        grid_size = self.params.grid_size
+        levels = self.params.grid_levels
+
+        for i in range(0, levels):
+            self.buy_levels.append(latest_price - (i+1) * grid_size)
+            self.sell_levels.append(latest_price + (i+1) * grid_size)
+
+    def next(self):
+        super().next()
+
+        current_price = self.data.close[0]
+        cash = self.broker.get_cash()
+        positions = self.getposition(self.data).size
+
+        for buy_level in self.buy_levels:
+            if current_price <= buy_level and buy_level not in self.order_dict:
+                if cash >= self.params.once_size * current_price:
+                    order = self.buy(size=self.params.once_size, price=current_price)
+                    self.order_dict[buy_level] = order
+                    self.log_debug(f"Will buy placed at {buy_level}, Cash: {cash}, Position: {positions}")
+                    self.update_stop_loss_point()
+
+        for sell_level in self.sell_levels:
+            if current_price >= sell_level and sell_level not in self.order_dict:
+                if self.need_stop_loss():
+                    order = self.sell()
+                    self.order_dict[sell_level] = order
+                    self.log_debug(f"Will sell placed at {sell_level}, Cash: {cash}, Position: {positions}")
+                    return
+                if positions >= self.params.once_size:
+                    order = self.sell(size=self.params.once_size, price=current_price)
+                    self.order_dict[sell_level] = order
+                    self.log_debug(f"Will sell placed at {sell_level}, Cash: {cash}, Position: {positions}")
+
+
+    def notify_order(self, order):
+        super().notify_order(order)
+
+        if order.status in [order.Completed]:
+            for key in list(self.order_dict.keys()):
+                if self.order_dict[key] == order:
+                    del self.order_dict[key]
