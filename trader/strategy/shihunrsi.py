55ba1491d951c8e497f8c773a1a6cdb9eb8b38da
outlier <outlier@chainer.tech>
2025-01-22 09:55:05 +0800

    fix:Unable to trade for shihun RIS strategy

--- trader/strategy/shihunrsi.py
+++ trader/strategy/shihunrsi.py
@@ -29,8 +29,10 @@ class ShihunRSIStrategy(TrilogyStrategy):
 
         if not self.position:
             if self.rsi[0] > self.params.overbought or self.canSell():
-                self.sell()
+                self.buy()
         else:
             if self.rsi[0] < self.params.oversold:
                 if self.canBuy():
-                    self.buy()
+                    self.sell()
+                elif self.need_stop_loss():
+                    self.sell()
