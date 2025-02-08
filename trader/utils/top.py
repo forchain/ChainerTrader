eae0432d5a520899cdd38e3adb28fb9f7263c42e
outlier <outlier@chainer.tech>
2025-02-08 19:51:31 +0800

    fix:get symbol must filter quote

--- trader/utils/top.py
+++ trader/utils/top.py
@@ -37,6 +37,8 @@ def write_symbols_top100_config(api_key:str,file_path="symbols_top100.txt",quote
         return False
     with open(get_file_path(file_path), "w", encoding="utf-8") as f:
         for sy in symbols:
+            if sy == quote:
+                continue
             f.write(sy + quote + "\n")
         return True
 
