29e5c96882f840ba317216524e3396145232b85a
outlier <outlier@chainer.tech>
2025-02-14 12:35:18 +0800

    feat:remove invalid symbols

--- trader/statistics/statistics.py
+++ trader/statistics/statistics.py
@@ -17,12 +17,6 @@ class Statistics:
         self.bts_list=[]
 
     def handler(self,msg:Message):
-        if self.cfg.stat == 0:
-            return
-        elif self.cfg.stat > 0:
-            if len(self.bts_list) > self.cfg.stat:
-                return
-
         self.log.info(f"handle message:{msg.name()}")
         add=False
         if isinstance(msg.data,BackTraderStat):
@@ -31,10 +25,16 @@ class Statistics:
         if isinstance(msg.data,TraderStat):
             self.bts_list.append(msg.data)
             add=True
-        if add and len(self.bts_list) > 1:
-            self.bts_list.sort(key=lambda bts: bts.tret.total_return_rate)
-            del_stat=self.bts_list.pop(0)
-            self.log.info(f"Remove item form stat list:{del_stat.strategy} {del_stat.symbol_interval} {del_stat.total_return_rate}")
+
+        if self.cfg.stat == 0:
+            return
+        elif self.cfg.stat > 0:
+            if len(self.bts_list) > self.cfg.stat:
+                if add and len(self.bts_list) > 1:
+                    self.bts_list.sort(key=lambda bts: bts.tret.total_return_rate)
+                    del_stat = self.bts_list.pop(0)
+                    self.log.info(
+                        f"Remove item form stat list:{del_stat.strategy} {del_stat.symbol_interval} {del_stat.tret.total_return_rate}")
 
     def report(self):
         if len(self.bts_list) > 0:
