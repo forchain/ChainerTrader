19dda5baa06a6250819bbbd7ffdfacc1a1b419f9
outlier <outlier@chainer.tech>
2025-02-19 11:51:29 +0800

    feat:support dual thrust strategy

--- trader/strategy/strategy.py
+++ trader/strategy/strategy.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from trader.strategy.boll_mean_reg import BollingerMeanRegStrategy
 from trader.strategy.dualma import DualMovingAverageStrategy
+from trader.strategy.dualthrust import DualThrustStrategy
 from trader.strategy.grid import GridStrategy
 from trader.strategy.macdrsi import MACDRSIStrategy
 from trader.strategy.shihunmacd import ShihunMACDStrategy
@@ -23,6 +24,7 @@ class StrategyType(Enum):
     BOLLMEANREG = 7       # Bollinger Bands Mean Regression Strategy
     TURTLE = 8            # Turtle: Richard Dennis and William Eckhardt
     DUALMA = 9            # Dual Moving Average Crossover Strategy
+    DUALTHRUST = 10       # Dual thrust strategy
 
 def parseStrategyType(name):
     if name == StrategyType.ShihunMACD.name:
@@ -45,6 +47,8 @@ def parseStrategyType(name):
         return StrategyType.TURTLE
     elif name == StrategyType.DUALMA.name:
         return StrategyType.DUALMA
+    elif name == StrategyType.DUALTHRUST.name:
+        return StrategyType.DUALTHRUST
     return None
 
 def parseStrategy(stype):
@@ -78,5 +82,7 @@ def parseStrategy(stype):
     elif stype == StrategyType.DUALMA:
         return DualMovingAverageStrategy
 
+    elif stype == StrategyType.DUALTHRUST:
+        return DualThrustStrategy
     else:
         return None
