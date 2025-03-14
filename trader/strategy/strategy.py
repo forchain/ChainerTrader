8a65f0c5480e7ade2faf812ab436884099e1666b
outlier <outlier@chainer.tech>
2025-03-14 20:43:13 +0800

    fix:strategy module name error

--- trader/strategy/strategy.py
+++ trader/strategy/strategy.py
@@ -1,22 +1,10 @@
-from enum import Enum
 
 from trader.common.common import dynamic_load
-from trader.strategy.boll_mean_reg import BollingerMeanRegStrategy
-from trader.strategy.dualma import DualMovingAverageStrategy
-from trader.strategy.dualthrust import DualThrustStrategy
-from trader.strategy.grid import GridStrategy
-from trader.strategy.kdj import KDJStrategy
-from trader.strategy.macdrsi import MACDRSIStrategy
-from trader.strategy.rsrs import RSRSStrategy
-from trader.strategy.shihunmacd import ShihunMACDStrategy
-from trader.strategy.shihunmacd2 import ShihunMACD2Strategy
-from trader.strategy.shihunmacdrsibb import ShihunMacdRsiBollingerBandStrategy
-from trader.strategy.shihunrsi import ShihunRSIStrategy
-from trader.strategy.shihunrsi2 import ShihunRSI2Strategy
-from trader.strategy.turtle import TurtleStrategy
+
 
 def parseStrategy(stype):
-    return dynamic_load(stype, get_strategy_class_name(stype))
+    mod=f"trader.strategy.{stype}"
+    return dynamic_load(mod, get_strategy_class_name(stype))
 
 def parse_strategys(stypes:[str]):
     ret=[]
