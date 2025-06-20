6ec3a40b1230ad49ba4d057b41ce0fb9eb2fa39c
outlier <outlier@chainer.tech>
2025-06-20 19:59:58 +0800

    feat:optimize deviation macd

--- src/trader/strategy/DeviationMACD/DeviationMACD.py
+++ src/trader/strategy/DeviationMACD/DeviationMACD.py
@@ -1,6 +1,10 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import math
+from collections import deque
+from enum import Enum
+
 from trader.common.config import DEFAULT_PERIOD
 from trader.strategy.base_strategy import BaseStrategy
 from trader.utils.operate import OperateType
@@ -9,11 +13,24 @@ import backtrader as bt
 from trader.utils.pivot_high import PivotHigh
 from trader.utils.pivot_low import PivotLow
 
+class Pivot:
+    def __init__(self,price:float,macd:float,bar_idx:int):
+        self.price=price
+        self.macd=macd
+        self.bar_idx=bar_idx
+
+class SourceType(Enum):
+    CLOSE = 0
+    HIGH = 1
+    LOW = 2
 
 class DeviationMACDStrategy(BaseStrategy):
     params = (
         ("lookback",20),
-        ("maxpp", 10)
+        ("maxpp", 10),
+        ("dontconfirm",False),
+        ("source",SourceType.CLOSE), # close high low
+        ("maxbars",100)
     )
 
     def __init__(self):
@@ -21,47 +38,41 @@ class DeviationMACDStrategy(BaseStrategy):
         self.set_default_period(12)
         self.macd_hist = bt.indicators.MACDHisto(self.data)
 
-        self.price_lows = []
-        self.macd_lows = []
-        self.price_highs = []
-        self.macd_highs = []
 
-        self.ph = PivotHigh(self.data.high, left=self.params.period, right=self.params.period)
-        self.pl = PivotLow(self.data.low, left=self.params.period, right=self.params.period)
+        self.ph = PivotHigh(self.get_pivot_source(), left=self.params.period, right=self.params.period)
+        self.pl = PivotLow(self.get_pivot_source(), left=self.params.period, right=self.params.period)
+        self.ph_pivots = deque(maxlen=self.params.maxpp)
+        self.pl_pivots = deque(maxlen=self.params.maxpp)
+        if self.params.dontconfirm:
+            self.startpoint=0
+        else:
+            self.startpoint=-1
+
+    def get_pivot_source(self):
+        if self.params.source == SourceType.CLOSE:
+            return self.data.close
+        elif self.params.source == SourceType.HIGH:
+            return self.data.high
+        else:
+            return self.data.low
 
     def next(self):
         super().next()
         if self.order:
             return
 
+        if not math.isnan(self.ph.middle_value()):
+            self.ph_pivots.appendleft(Pivot(self.ph.middle_value(),self.macd_hist[self.ph.middle_idx()],self.bar_idx()))
+        if not math.isnan(self.pl.middle_value()):
+            self.pl_pivots.appendleft(Pivot(self.pl.middle_value(),self.macd_hist[self.pl.middle_idx()],self.bar_idx()))
+
         willOpt = OperateType.UNKNOWN
 
-        price = self.data.close[0]
-        hist = self.macd_hist[0]
+        if self.positive_regular_positive_hidden_divergence(True) or self.positive_regular_positive_hidden_divergence(False):
+            willOpt=OperateType.BUY
 
-        if self.is_local_min(self.data.close, 2):
-            self.price_lows.append(price)
-            self.macd_lows.append(hist)
-        if self.is_local_max(self.data.close, 2):
-            self.price_highs.append(price)
-            self.macd_highs.append(hist)
-
-        self.price_lows = self.price_lows[-self.params.lookback:]
-        self.macd_lows = self.macd_lows[-self.params.lookback:]
-        self.price_highs = self.price_highs[-self.params.lookback:]
-        self.macd_highs = self.macd_highs[-self.params.lookback:]
-
-        if len(self.price_lows) >= 2 and len(self.macd_lows) >= 2:
-            p1, p2 = self.price_lows[-2], self.price_lows[-1]
-            m1, m2 = self.macd_lows[-2], self.macd_lows[-1]
-            if p2 < p1 and m2 > m1:
-                willOpt = OperateType.BUY
-
-        if len(self.price_highs) >= 2 and len(self.macd_highs) >= 2:
-            p1, p2 = self.price_highs[-2], self.price_highs[-1]
-            m1, m2 = self.macd_highs[-2], self.macd_highs[-1]
-            if p2 > p1 and m2 < m1:
-                willOpt = OperateType.SELL
+        if self.negative_regular_negative_hidden_divergence(True) or self.negative_regular_negative_hidden_divergence(False):
+            willOpt=OperateType.SELL
 
         if not self.position:
             if willOpt == OperateType.BUY:
@@ -73,18 +84,78 @@ class DeviationMACDStrategy(BaseStrategy):
                 self.log_info(f'Kline:{self.cur_datetime()}, 创建 卖单:{self.data.close[0]:.2f}')
                 self.order = self.sell()
 
-
-
-    def is_local_min(self, data, look=2):
-        return data[-look] > data[-1] and data[0] < data[-1]
-
-    def is_local_max(self, data, look=2):
-        return data[-look] < data[-1] and data[0] > data[-1]
-
-    def positive_regular_positive_hidden_divergence(self) -> bool:
-        if self.data.close[0] <= self.data.close[-1]:
+    def positive_regular_positive_hidden_divergence(self,pr:bool) -> bool:
+        if self.bar_idx() <=5:
             return False
-        for i in range(self.params.maxpp):
-            len = bar_index - array.get(pl_positions, x) + period
 
-        return True
+        if not self.params.dontconfirm and self.macd_hist[0] <= self.macd_hist[-1] and self.data.close[0] <= self.data.close[-1]:
+            return False
+
+        price=self.get_pivot_source()[self.startpoint]
+        macdh=self.macd_hist[self.startpoint]
+        for item in self.ph_pivots:
+            if (self.bar_idx()-item.bar_idx) > self.params.maxbars:
+                break
+            need=False
+            if pr and macdh > item.macd and price < item.price:
+                need=True
+            elif (not pr) and macdh < item.macd and price > item.price:
+                need=True
+            if need:
+                leng=self.bar_idx()-item.bar_idx
+
+                slope1 = (macdh - price) / (leng - self.startpoint)
+                virtual_line1 = macdh - slope1
+                slope2 = (self.data.close[self.startpoint] - self.data.close[-leng]) / (leng - self.startpoint)
+                virtual_line2 = self.data.close[self.startpoint] - slope2
+                arrived = True
+
+                for y in range(1 + self.startpoint,leng-1):
+                    if self.macd_hist[-y] < virtual_line1 or self.data.close[-y] < virtual_line2:
+                        arrived = False
+                        break
+                    virtual_line1 = virtual_line1 - slope1
+                    virtual_line2 = virtual_line2 - slope2
+
+                if arrived:
+                    return True
+
+        return False
+
+    def negative_regular_negative_hidden_divergence(self,pr:bool) -> bool:
+        if self.bar_idx() <=5:
+            return False
+
+        if not self.params.dontconfirm and self.macd_hist[0] >= self.macd_hist[-1] and self.data.close[0] >= self.data.close[-1]:
+            return False
+
+        price=self.get_pivot_source()[self.startpoint]
+        macdh=self.macd_hist[self.startpoint]
+        for item in self.pl_pivots:
+            if (self.bar_idx()-item.bar_idx) > self.params.maxbars:
+                break
+            need=False
+            if pr and macdh < item.macd and price > item.price:
+                need=True
+            elif (not pr) and macdh > item.macd and price < item.price:
+                need=True
+            if need:
+                leng=self.bar_idx()-item.bar_idx
+
+                slope1 = (macdh - price) / (leng - self.startpoint)
+                virtual_line1 = macdh - slope1
+                slope2 = (self.data.close[self.startpoint] - self.data.close[-leng]) / (leng - self.startpoint)
+                virtual_line2 = self.data.close[self.startpoint] - slope2
+                arrived = True
+
+                for y in range(1 + self.startpoint,leng-1):
+                    if self.macd_hist[-y] > virtual_line1 or self.data.close[-y] > virtual_line2:
+                        arrived = False
+                        break
+                    virtual_line1 = virtual_line1 - slope1
+                    virtual_line2 = virtual_line2 - slope2
+
+                if arrived:
+                    return True
+
+        return False
