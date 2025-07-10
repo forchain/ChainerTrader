167067dd6f9d8fc5195113904770d1fad5147b83
outlier <outlier@chainer.tech>
2025-07-10 09:55:02 +0800

    fix:corruption when pivot high and low indicator

--- src/trader/utils/pivot_low.py
+++ src/trader/utils/pivot_low.py
@@ -13,8 +13,8 @@ class PivotLow(bt.Indicator):
         self.addminperiod(self.window_size)
 
     def next(self):
-        if len(self.data) < self.window_size:
-            self.lines.pivotlow[0] = float('nan')
+        self.lines.pivotlow[0] = float('nan')
+        if len(self.data) <= self.window_size:
             return
 
         mid_idx = self.middle_idx()
