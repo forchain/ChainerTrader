11b3f8279599fc56ed0062439d2e3b4a91a8ad98
outlier <outlier@chainer.tech>
2025-07-21 15:07:16 +0800

    refactor:trend_a

--- src/trader/indicators/trend_a.py
+++ src/trader/indicators/trend_a.py
@@ -1,7 +1,7 @@
 import backtrader as bt
 
 from trader.utils.ma import MAType
-
+from backtrader.indicators import ALMA
 
 def get_ma(data, period, ma_type, alma_offset=0.85, alma_sigma=6):
     if ma_type == MAType.EMA:
@@ -13,7 +13,6 @@ def get_ma(data, period, ma_type, alma_offset=0.85, alma_sigma=6):
     elif ma_type == MAType.HMA:
         return bt.ind.HullMovingAverage(data, period=period)
     elif ma_type == MAType.ALMA:
-        from backtrader.indicators import ALMA
         return ALMA(data, period=period, offset=alma_offset, sigma=alma_sigma)
     else:
         return bt.ind.EMA(data, period=period)
