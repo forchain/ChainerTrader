85d2e42a9831e30a7ebb14229892d903f0e361c6
outlier <outlier@chainer.tech>
2025-08-06 12:41:03 +0800

    feat:optimize config

--- src/trader/common/config.py
+++ src/trader/common/config.py
@@ -6,119 +6,111 @@ from trader.utils.trend import parseTrendType
 
 DEFAULT_PERIOD = 14
 
+TRADER_COMMISSION = "TRADER_COMMISSION"
+TRADER_ATR = "TRADER_ATR"
+TRADER_STOPLOSS = "TRADER_STOPLOSS"
+TRADER_PERIOD = "TRADER_PERIOD"
+TRADER_LOG_FILE = "TRADER_LOG_FILE"
+TRADER_PLOT = "TRADER_PLOT"
+TRADER_MODE = "TRADER_MODE"
+TRADER_LOG_LEVEL = "TRADER_LOG_LEVEL"
+TRADER_EXCHANGE = "TRADER_EXCHANGE"
+TRADER_DB_URI = "TRADER_DB_URI"
+TRADER_DB_NAME = "TRADER_DB_NAME"
+TRADER_WINDOW = "TRADER_WINDOW"
+TRADER_TASKS = "TRADER_TASKS"
+TRADER_CASH = "TRADER_CASH"
+TRADER_STAT = "TRADER_STAT"
+TRADER_NOTICE = "TRADER_NOTICE"
+TRADER_API = "TRADER_API"
+
+
 class Config:
-    def __init__(self,commission=0.001,
-                      atr=True,
-                      stoploss=False,
-                      period=DEFAULT_PERIOD,
-                      log_file=False,
-                      plot=False,
-                      mode=None,
-                      log_level="INFO",
-                      exchange=None,
-                      db_uri=None,
-                      db_name=NAME,
-                      window=1000,
-                      tasks=None,
-                      cash=100000,
-                      stat=50,
-                      notice=None,
-                      api=None):
-        self.mode=parseTrendType(mode)
-        self.commission=commission
-        self.atr=atr
-        self.stoploss=stoploss
-        self.period=period
-        self.log_file=log_file
-        self.plot=plot
-        self.log_level=log_level
-        self.exchange=exchange
-        self.db_uri=db_uri
-        self.db_name=db_name
-        self.window=window
-        self.tasks=tasks
-        self.cash=cash
-        self.stat=stat
-        self.notice=notice
-        self.api=api
+    def __init__(
+        self,
+        commission=0.001,
+        atr=True,
+        stoploss=False,
+        period=DEFAULT_PERIOD,
+        log_file=False,
+        plot=False,
+        mode=None,
+        log_level="INFO",
+        exchange=None,
+        db_uri=None,
+        db_name=NAME,
+        window=1000,
+        tasks=None,
+        cash=100000,
+        stat=50,
+        notice=None,
+        api=None,
+    ):
+        self.commission = commission
+        self.atr = atr
+        self.stoploss = stoploss
+        self.period = period
+        self.log_file = log_file
+        self.plot = plot
+        self.mode = parseTrendType(mode)
+        self.log_level = log_level
+        self.exchange = exchange
+        self.db_uri = db_uri
+        self.db_name = db_name
+        self.window = window
+        self.tasks = tasks
+        self.cash = cash
+        self.stat = stat
+        self.notice = notice
+        self.api = api
 
     def exportEnv(self):
-        os.environ['commission'] = str(self.commission)
-        os.environ['atr'] = str(self.atr)
-        os.environ['stoploss'] = str(self.stoploss)
-        os.environ['period'] = str(self.period)
-        os.environ['log_file'] = str(self.log_file)
-        os.environ['plot'] = str(self.plot)
-        os.environ['mode'] = self.mode.name
-        os.environ['log_level'] = self.log_level
+        os.environ[TRADER_COMMISSION] = str(self.commission)
+        os.environ[TRADER_ATR] = str(self.atr)
+        os.environ[TRADER_STOPLOSS] = str(self.stoploss)
+        os.environ[TRADER_PERIOD] = str(self.period)
+        os.environ[TRADER_LOG_FILE] = str(self.log_file)
+        os.environ[TRADER_PLOT] = str(self.plot)
+        os.environ[TRADER_MODE] = self.mode.name
+        os.environ[TRADER_LOG_LEVEL] = self.log_level
 
         if self.exchange:
-            os.environ['exchange'] = self.exchange
+            os.environ[TRADER_EXCHANGE] = self.exchange
         if self.db_uri:
-            os.environ['db_uri'] = self.db_uri
-        os.environ['db_name'] = self.db_name
-        os.environ['window'] = str(self.window)
+            os.environ[TRADER_DB_URI] = self.db_uri
+        os.environ[TRADER_DB_NAME] = self.db_name
+        os.environ[TRADER_WINDOW] = str(self.window)
         if self.tasks:
-            os.environ['tasks'] = self.tasks
-        os.environ['cash'] = str(self.cash)
-        os.environ['stat'] = str(self.stat)
-        os.environ['notice'] = str(self.notice)
-        os.environ['api'] = str(self.api)
+            os.environ[TRADER_TASKS] = self.tasks
+        os.environ[TRADER_CASH] = str(self.cash)
+        os.environ[TRADER_STAT] = str(self.stat)
+        os.environ[TRADER_NOTICE] = str(self.notice)
+        os.environ[TRADER_API] = str(self.api)
 
     def to_dict(self):
         return {
-            'commission':self.commission,
-            'atr':self.atr,
-            'stoploss':self.stoploss,
-            'period':self.period,
-            'log_file':self.log_file,
-            'plot':self.plot,
-            'mode':self.mode.name,
-            'log_level':self.log_level,
-            'exchange':self.exchange,
-            'db_uri': self.db_uri,
-            'db_name': self.db_name,
-            'window': self.window,
-            'tasks':self.tasks,
-            'cash':self.cash,
-            'stat':self.stat,
-            'notice':self.notice,
-            'api': self.api
+            "commission": self.commission,
+            "atr": self.atr,
+            "stoploss": self.stoploss,
+            "period": self.period,
+            "log_file": self.log_file,
+            "plot": self.plot,
+            "mode": self.mode.name,
+            "log_level": self.log_level,
+            "exchange": self.exchange,
+            "db_uri": self.db_uri,
+            "db_name": self.db_name,
+            "window": self.window,
+            "tasks": self.tasks,
+            "cash": self.cash,
+            "stat": self.stat,
+            "notice": self.notice,
+            "api": self.api,
         }
 
-    def get_log_level(self)->int:
+    def get_log_level(self) -> int:
         return logging.getLevelName(self.log_level)
 
-def NewConfigFromEnv():
-    commission = os.environ.get('commission')
-    if commission is None:
-        commission="0"
-    period = os.environ.get('period')
-    if period is None:
-        period="0"
-    window = os.environ.get('window')
-    if window is None:
-        window="0"
 
-    return Config(
-        float(commission),
-        bool(os.environ.get('atr')),
-        bool(os.environ.get('stoploss')),
-        int(period),
-        bool(os.environ.get('log_file')),
-        bool(os.environ.get('plot')),
-        os.environ.get('mode'),
-        os.environ.get('log_level'),
-        os.environ.get('exchange'),
-        os.environ.get('db_uri'),
-        os.environ.get('db_name'),
-        int(window),
-        os.environ.get('tasks'),
-        os.environ.get('cash'),
-        os.environ.get('stat'),
-        os.environ.get('notice'),
-        os.environ.get('api')
-    )
-
-def default()->Config:
+def default() -> Config:
     return Config()
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import os
 
-from trader.common.config import Config
+from trader.common.config import Config, NewConfigFromEnv
 from trader.common.path import GetScriptsDir
 from trader.task.task_config import TaskConfig, get_symbols, parse_task_config
 from trader.task.task_type import TaskType
@@ -11,31 +11,47 @@ def test_config():
     cfg = Config()
     print(cfg.to_dict())
 
+
 def test_symbols():
-    cfgs = [TaskConfig(TaskType.TRADER,SymbolInterval("BTCUSDT",Interval.INTERVAL_1d)),
-            TaskConfig(TaskType.TRADER,SymbolInterval("ETHUSDT",Interval.INTERVAL_1d))]
+    cfgs = [
+        TaskConfig(TaskType.TRADER, SymbolInterval("BTCUSDT", Interval.INTERVAL_1d)),
+        TaskConfig(TaskType.TRADER, SymbolInterval("ETHUSDT", Interval.INTERVAL_1d)),
+    ]
     print(get_symbols(cfgs))
 
+
 def test_symbols_intervals():
-    cfgs = [TaskConfig(TaskType.TRADER, SymbolInterval("BTCUSDT", Interval.INTERVAL_1d)),
-            TaskConfig(TaskType.TRADER, SymbolInterval("ETHUSDT", Interval.INTERVAL_1d))]
+    cfgs = [
+        TaskConfig(TaskType.TRADER, SymbolInterval("BTCUSDT", Interval.INTERVAL_1d)),
+        TaskConfig(TaskType.TRADER, SymbolInterval("ETHUSDT", Interval.INTERVAL_1d)),
+    ]
     for si in cfgs:
         print(si.symbol_interval.name())
 
+
 def test_taskconfig():
-    file=os.path.join(GetScriptsDir(),"multi_backtrader.json")
-    tcfgs=parse_task_config(file)
+    file = os.path.join(GetScriptsDir(), "multi_backtrader.json")
+    tcfgs = parse_task_config(file)
     for tcfg in tcfgs:
         print(tcfg.to_dict())
+
 
 def test_taskconfig_uk():
-    file=os.path.join(GetScriptsDir(),"update_klines.json")
-    tcfgs=parse_task_config(file)
+    file = os.path.join(GetScriptsDir(), "update_klines.json")
+    tcfgs = parse_task_config(file)
     for tcfg in tcfgs:
         print(tcfg.to_dict())
 
+
 def test_taskconfig_ckn():
-    file=os.path.join(GetScriptsDir(),"check_klines_num.json")
-    tcfgs=parse_task_config(file)
+    file = os.path.join(GetScriptsDir(), "check_klines_num.json")
+    tcfgs = parse_task_config(file)
     for tcfg in tcfgs:
         print(tcfg.to_dict())
+
+
+def test_config_from_env():
+    cfg = Config()
+    cfg.exportEnv()
+    ncfg = NewConfigFromEnv()
+    print(ncfg.to_dict())
