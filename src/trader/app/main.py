1874d91487e90e2fd946fc4765adc897f90bf258
outlier <outlier@chainer.tech>
2025-08-07 12:27:57 +0800

    feat:format code

--- src/trader/app/main.py
+++ src/trader/app/main.py
@@ -9,54 +9,118 @@ def main():
     parser = argparse.ArgumentParser(
         description="Implement TradvingView Algorithms of Youtube Channel Shi Hun",
         epilog="Chainer Labs",
-        fromfile_prefix_chars='@')
+        fromfile_prefix_chars="@",
+    )
 
-    parser.add_argument("-v", "--version",help="Version",action="store_true")
-    parser.add_argument('--period', help='Period for the moving average',action='store',type=int, default=14,required=False)
-    parser.add_argument('--commission', help='Transaction commission', action='store', type=float, default=0.001,required=False)
-    parser.add_argument("--atr", help="Use atr for stop-loss-point", action="store_true")
+    parser.add_argument("-v", "--version", help="Version", action="store_true")
+    parser.add_argument(
+        "--period",
+        help="Period for the moving average",
+        action="store",
+        type=int,
+        default=14,
+        required=False,
+    )
+    parser.add_argument(
+        "--commission",
+        help="Transaction commission",
+        action="store",
+        type=float,
+        default=0.001,
+        required=False,
+    )
+    parser.add_argument(
+        "--atr", help="Use atr for stop-loss-point", action="store_true"
+    )
     parser.add_argument("--stoploss", help="Use stop-loss-point", action="store_true")
-    parser.add_argument("--api", help="Start the Web API service with optional binding address and port (e.g. 127.0.0.1:8000, :8000, 127.0.0.1)", type=str, const="127.0.0.1:8000", nargs="?")
+    parser.add_argument(
+        "--api",
+        help="Start the Web API service with optional binding address and port (e.g. 127.0.0.1:8000, :8000, 127.0.0.1)",
+        type=str,
+        const="127.0.0.1:8000",
+        nargs="?",
+    )
     parser.add_argument("--log_file", help="Write log to file", action="store_true")
     parser.add_argument("--plot", help="Plot data", action="store_true")
-    parser.add_argument("--mode", help="trend type: NORMAL UP DOWN",type=str)
-    parser.add_argument("--log_level", help="logger display level:CRITICAL,FATAL,ERROR,WARNING,WARNING,INFO,DEBUG", type=str,default="INFO")
-    parser.add_argument("--exchange", help="Which remote exchange is connected to:BINANCE",type=str)
+    parser.add_argument("--mode", help="trend type: NORMAL UP DOWN", type=str)
+    parser.add_argument(
+        "--log_level",
+        help="logger display level:CRITICAL,FATAL,ERROR,WARNING,WARNING,INFO,DEBUG",
+        type=str,
+        default="INFO",
+    )
+    parser.add_argument(
+        "--exchange", help="Which remote exchange is connected to:BINANCE", type=str
+    )
     parser.add_argument("--db", help="Enable database for MongoDB", action="store_true")
-    parser.add_argument("--db_uri", help="Database URI for MongoDB", type=str, default="mongodb://localhost:27017/")
-    parser.add_argument("--db_name", help="Database name for MongoDB", type=str,default="trader")
-    parser.add_argument('--window', help='Window for backtesting', action='store', type=int, default=1000)
-    parser.add_argument("--tasks", help="Tasks config:TRADER,BACK_TRADER,UPDATE_KLINES,CHECK_KLINES,IMPORT_CSV",type=str)
-    parser.add_argument('--cash', help='Init cash for backtesting', action='store', type=int, default=100000)
-    parser.add_argument('--stat', help='The maximum number of entries displayed in statistics for backtesting', action='store', type=int, default=50)
-    parser.add_argument("--notice", help="Load notification configuration files, such as email", type=str)
+    parser.add_argument(
+        "--db_uri",
+        help="Database URI for MongoDB",
+        type=str,
+        default="mongodb://localhost:27017/",
+    )
+    parser.add_argument(
+        "--db_name", help="Database name for MongoDB", type=str, default="trader"
+    )
+    parser.add_argument(
+        "--window",
+        help="Window for backtesting",
+        action="store",
+        type=int,
+        default=1000,
+    )
+    parser.add_argument(
+        "--tasks",
+        help="Tasks config:TRADER,BACK_TRADER,UPDATE_KLINES,CHECK_KLINES,IMPORT_CSV",
+        type=str,
+    )
+    parser.add_argument(
+        "--cash",
+        help="Init cash for backtesting",
+        action="store",
+        type=int,
+        default=100000,
+    )
+    parser.add_argument(
+        "--stat",
+        help="The maximum number of entries displayed in statistics for backtesting",
+        action="store",
+        type=int,
+        default=50,
+    )
+    parser.add_argument(
+        "--notice",
+        help="Load notification configuration files, such as email",
+        type=str,
+    )
 
     args = parser.parse_args()
     db_uri = None
     if args.db:
-        db_uri=args.db_uri
+        db_uri = args.db_uri
 
-    cfg = new_and_env(args.commission,
-                 args.atr,
-                 args.stoploss,
-                 args.period,
-                 args.log_file,
-                 args.plot,
-                 args.mode,
-                 args.log_level,
-                 args.exchange,
-                 db_uri,
-                 args.db_name,
-                 args.window,
-                 args.tasks,
-                 args.cash,
-                 args.stat,
-                 args.notice,
-                 args.api)
+    cfg = new_and_env(
+        args.commission,
+        args.atr,
+        args.stoploss,
+        args.period,
+        args.log_file,
+        args.plot,
+        args.mode,
+        args.log_level,
+        args.exchange,
+        db_uri,
+        args.db_name,
+        args.window,
+        args.tasks,
+        args.cash,
+        args.stat,
+        args.notice,
+        args.api,
+    )
     if args.version:
         print(version())
         return
-
 
     if cfg.api:
         start(cfg)
