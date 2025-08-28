8bc9069ea94669f139fe9c2b58e46e481d34ba3c
outlier <outlier@chainer.tech>
2025-08-28 11:27:30 +0800

    feat:add delete task for api

--- src/trader/rpc/api/task.py
+++ src/trader/rpc/api/task.py
@@ -6,3 +6,8 @@ router = APIRouter()
 @router.get("")
 def task(id: int, request: Request):
     return request.app.state.app.db_manager.task.get_task(id)
+
+
+@router.delete("")
+def task(id: int, request: Request):
+    pass
