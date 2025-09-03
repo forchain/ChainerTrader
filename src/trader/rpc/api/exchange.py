ce6fcef3fc034b28bc063833dfde012cddf65f9f
outlier <outlier@chainer.tech>
2025-09-03 12:41:56 +0800

    feat:exchange rpc

--- src/trader/rpc/api/exchange.py
+++ src/trader/rpc/api/exchange.py
@@ -4,7 +4,7 @@ router = APIRouter()
 
 
 @router.get("")
-def exchange_info(symbol: str, request: Request):
+def exchange(symbol: str, request: Request):
     if len(symbol) <= 0:
         return {"error": "must config symbol"}
     return request.app.state.app.exchange.get_exchange_info(symbol)
